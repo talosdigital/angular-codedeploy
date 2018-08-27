fdb55c64e15b3d63c475f56962e91815ecd5638e
Daniel Martinez <daniel.martinez@talosdigital.com>
2018-08-27 17:53:51 -0500

    Fixing file error with codedeploy

--- deployment/scripts/system_configure.sh
+++ deployment/scripts/system_configure.sh
@@ -10,7 +10,7 @@ mkdir -p /etc/httpd/sites-enabled
 #copy configuration
 cp $CODEDEPLOY/deployment/conf/talos.conf /etc/httpd/conf.d/
 envsubst < $CODEDEPLOY/deployment/conf/template.conf > /etc/httpd/sites-available/$PROJECT.conf
-envsubst < $CODEDEPLOY/deployment/scripts/build_application_template.sh > $CODEDEPLOY/deployment/scripts/build_application.sh
+envsubst < $CODEDEPLOY/deployment/conf/build_application_template.sh >> $CODEDEPLOY/deployment/scripts/build_application.sh
 ln -sfn /etc/httpd/sites-available/$PROJECT.conf /etc/httpd/sites-enabled/$PROJECT.conf
 
 #Add Firewall Rule for External Access
