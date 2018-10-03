b86a525ed4514217257e502f4c102c4649293e33
Daniel Martinez <dmartinezmont@gmail.com>
2018-10-03 13:24:06 -0500

    Change template file to be more general

--- deployment/scripts/setenv.sh
+++ deployment/scripts/setenv.sh
@@ -5,4 +5,5 @@ export CODEDEPLOY=/opt/angular-codedeploy
 # Target
 cd $CODEDEPLOY
 export PROJECT=$(ls -1 target-* | sed -e 's/target-//g')
+export PROJECT_WITHOUT_WWW=$(echo $PROJECT | sed 's/www\.//g')
 export RUNSCRIPT=$(ls -1 script-* | sed -e 's/script-//g')
