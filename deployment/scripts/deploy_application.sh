35f19491ee4ab03b96a608cd0675ad59e6dc2ab2
Daniel Martínez <dmartinezmont@gmail.com>
2019-03-18 16:34:42 -0500

    Add support for .htaccess file

--- deployment/scripts/deploy_application.sh
+++ deployment/scripts/deploy_application.sh
@@ -7,9 +7,11 @@ then
     rm -rf /var/www/$PROJECT
     mkdir -p /var/www/$PROJECT
     cp -r $BUILDDIRECTORY/* /var/www/$PROJECT
+    cp $CODEDEPLOY/.htaccess /var/www/$PROJECT
     chown -R apache:apache /var/www/$PROJECT
 else 
     rm -rf html/*
     cp -r $BUILDDIRECTORY/* /var/www/html
+    cp $CODEDEPLOY/.htaccess /var/www/html
     chown -R apache:apache /var/www/html
 fi
